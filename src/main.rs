ba9a71867c57772f2c04e946e774a395f1fda692
Vzaa <Vzaa@users.noreply.github.com>
2018-08-28 16:15:47 +0300

    Updates

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ extern crate itertools;
 extern crate serde_derive;
 extern crate serde_json;
 
-use std::io::BufReader;
-use std::fs::File;
-use std::f64;
-use std::env;
 use std::collections::HashMap;
+use std::env;
+use std::f64;
+use std::fs::File;
+use std::io::BufReader;
 
 use itertools::{Itertools, MinMaxResult};
 
@@ -19,6 +19,8 @@ struct ItemEntry(f64, Option<Vec<String>>);
 struct ItemList {
     items: Vec<f64>,
 }
+
+const ROUND_TO: f64 = 0.25;
 
 impl ItemList {
     pub fn new() -> ItemList {
@@ -49,12 +51,12 @@ fn price_cut(tot: f64) -> f64 {
     }
 }
 
-fn round4(x: f64) -> f64 {
-    (x * 4.0).round() / 4.0
+fn round_scaled(x: f64) -> f64 {
+    (x * (1.0 / ROUND_TO)).round() / (1.0 / ROUND_TO)
 }
 
-fn ceil4(x: f64) -> f64 {
-    (x * 4.0).ceil() / 4.0
+fn ceil_scaled(x: f64) -> f64 {
+    (x * (1.0 / ROUND_TO)).ceil() / (1.0 / ROUND_TO)
 }
 
 fn yscalc(dataset: &HashMap<String, ItemList>) {
@@ -65,10 +67,10 @@ fn yscalc(dataset: &HashMap<String, ItemList>) {
     let ratio = sum_after / sum_before;
 
     let pay: Vec<f64> = entries.iter().map(|e| e.sum() * ratio).collect();
-    let mut pay_rounded: Vec<f64> = pay.iter().map(|c| round4(*c)).collect();
+    let mut pay_rounded: Vec<f64> = pay.iter().map(|c| round_scaled(*c)).collect();
     let rounded_sum: f64 = pay_rounded.iter().sum();
 
-    let mut remainder = ceil4(sum_after) - rounded_sum;
+    let mut remainder = ceil_scaled(sum_after) - rounded_sum;
 
     loop {
         let minmax = izip!(&mut pay_rounded, &pay)
@@ -78,19 +80,15 @@ fn yscalc(dataset: &HashMap<String, ItemList>) {
         if let MinMaxResult::MinMax((_, min), (_, max)) = minmax {
             let tgt = if remainder > 0.0 { min } else { max };
 
-            let (adj, done) = if remainder >= 0.25 {
-                (0.25, false)
-            } else if remainder <= -0.25 {
-                (-0.25, false)
+            let adj = if remainder >= ROUND_TO {
+                ROUND_TO
+            } else if remainder <= -ROUND_TO {
+                -ROUND_TO
             } else if remainder > 0.0 {
-                (remainder, false)
+                remainder
             } else {
-                (0.0, true)
-            };
-
-            if done {
                 break;
-            }
+            };
 
             *tgt += adj;
             remainder -= adj;
@@ -110,16 +108,16 @@ fn yscalc(dataset: &HashMap<String, ItemList>) {
         println!("  {:.2} ({:.2})\n", r, p);
     }
 
-    let payed_sum = pay_rounded.iter().sum::<f64>();
-    println!("Sum: {:.2}", payed_sum);
-    println!("Remainder: {:.2}", sum_after - payed_sum);
+    let paid_sum = pay_rounded.iter().sum::<f64>();
+    println!("Sum: {:.2}", paid_sum);
+    println!("Remainder: {:.2}", sum_after - paid_sum);
 }
 
-fn items_to_hmap(items: Vec<ItemEntry>) -> HashMap<String, ItemList> {
+fn items_to_hmap(items: &[ItemEntry]) -> HashMap<String, ItemList> {
     let mut dataset: HashMap<String, ItemList> = HashMap::new();
 
     // Init HashMap with unique names
-    for &ItemEntry(_, ref n) in &items {
+    for &ItemEntry(_, ref n) in items {
         if let Some(ref names) = *n {
             for name in names {
                 dataset.entry(name.to_owned()).or_insert_with(ItemList::new);
@@ -128,7 +126,7 @@ fn items_to_hmap(items: Vec<ItemEntry>) -> HashMap<String, ItemList> {
     }
 
     // Distribute items
-    for &ItemEntry(v, ref n) in &items {
+    for &ItemEntry(v, ref n) in items {
         if let Some(ref names) = *n {
             let v_div = v / names.len() as f64;
 
@@ -154,7 +152,7 @@ fn main() {
         .map(|f| serde_json::from_reader(BufReader::new(f)).unwrap())
         .unwrap();
 
-    let dataset = items_to_hmap(items);
+    let dataset = items_to_hmap(&items);
 
     yscalc(&dataset);
 }
