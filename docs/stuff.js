4d4d780cf0e7b41766d93902a82b1deccc8cf33a
Vzaa <Vzaa@users.noreply.github.com>
2019-03-07 17:12:14 +0300

    Test gh pages

---
+++ docs/stuff.js
@@ -0,0 +1,173 @@
+Vue.component('person-entry', {
+  props: ['name', 'id'],
+  template: `
+    <button
+    class="btn btn-outline-info"
+    @click="del_name(id)">
+      {{ name }}
+      <span class="badge badge-danger">x</span>
+    </button>
+  `,
+  methods: {
+    del_name: function(id) {
+      this.$emit('delet-person', id)
+    }
+  }
+})
+
+Vue.component('result-entry', {
+  props: ['res', 'id'],
+  template: `
+    <tr>
+      <td>
+      {{ res.name }}
+      </td>
+      <td>
+      {{ res.total.toFixed(2) }}
+      </td>
+      <td>
+      {{res.sum().toFixed(2)}} ({{ res.item_str() }})
+      </td>
+    </tr>
+  `,
+  methods: {
+  }
+})
+
+Vue.component('item-entry', {
+  props: ['price', 'shared', 'id', 'persons'],
+  template: `
+  <tr>
+    <td scope="col" width="5%">
+      {{ id + 1 }}
+    </td>
+    <td scope="col" width="5%">
+      <span class="badge badge-primary">{{ price.toFixed(2) }}</span>
+    </td>
+    <td scope="col" width="25%">
+      <select class="custom-select" @change="add_shared(id, $event)">
+        <option disabled selected value="">Add</option>
+        <option v-for="p in persons">{{ p }}</option>
+      </select>
+    </td>
+    <td scope="col">
+      <span class="badge badge-secondary" v-if="shared.length == 0">Everyone</span>
+      <button
+        class="btn btn-sm btn-outline-info"
+        @click="del_shared(id, name)"
+        v-for="name in shared"
+        >
+        {{ name }}
+      <span class="badge badge-danger">x</span>
+        </button>
+    </td>
+    <td scope="col" width="5%">
+      <button class="btn btn-danger" @click="del_item(id)">
+        Del
+      </button>
+    </td>
+  </tr>
+  `,
+  methods: {
+    add_shared: function(id, e) {
+      this.$emit('add-shared', id, e.target.value)
+      e.target.selectedIndex = 0
+    },
+    del_shared: function(id, name) {
+      this.$emit('delet-shared', id, name)
+    },
+    del_item: function(id) {
+      this.$emit('delet-item', id)
+    }
+  },
+})
+
+var app = new Vue({
+                  el: '#app',
+                  data: {
+                    persons: [],
+                    items: [],
+                  },
+                  methods: {
+                    add_person: function() {
+                      var name = this.$refs.name_input.value
+
+                      if (name.trim() == "") {
+                        return
+                      }
+
+                      if (this.persons.indexOf(name) == -1) {
+                        this.persons.push(name.trim())
+                      }
+                      this.$refs.name_input.value = ""
+                    },
+                    add_item: function() {
+                      var price = Number(this.$refs.item_input.value)
+                      if (price <= 0 ) {
+                        this.$refs.item_input.value = ""
+                        return
+                      }
+                      var item = { price: price, shared: [] }
+                      this.items.push(item)
+                      this.$refs.item_input.value = ""
+                    },
+                    del_person: function(id) {
+                      var name = this.persons[id]
+                      this.persons.splice(id, 1)
+
+                      this.items.forEach(function(item) {
+                        item.shared = item.shared.filter(n => n !== name)
+                      })
+
+                    },
+                    del_item: function(id) {
+                      this.items.splice(id, 1)
+                    },
+                    add_shared: function(id, name) {
+                      if (this.items[id].shared.indexOf(name) == -1) {
+                        this.items[id].shared.push(name)
+                      }
+                    },
+                    del_shared: function(id, name) {
+                      this.items[id].shared = this.items[id].shared.filter(n => n !== name)
+                    },
+
+                  },
+                  computed: {
+                    calc: function() {
+                      return testan(this.persons, this.items)
+                    },
+                    paid: function() {
+                      var paid = this.calc.map(function (x) { return x.total; }).reduce(function (a, b) { return a + b; }, 0).toFixed(2)
+                      return paid
+                    },
+                    org_before: function() {
+                      var paid = this.items.map(function (x) { return x.price; }).reduce(function (a, b) { return a + b; }, 0)
+                      return paid
+                    },
+                    org_sum: function() {
+                      return (this.org_before - price_cut(this.org_before)).toFixed(2)
+                    },
+                    tip: function() {
+                      return (this.paid - this.org_sum).toFixed(2)
+                    },
+                    ratio: function() {
+                      return (this.paid / this.org_before).toFixed(2)
+                    },
+                    to_json: function() {
+                      var as_arr = [];
+
+                      this.items.forEach(function(item) {
+                        if (item.shared.length == 0) {
+                          as_arr.push([item.price, null])
+                        } else {
+                          as_arr.push([item.price, item.shared])
+                        }
+                      })
+
+                      //return JSON.stringify(as_arr, null, 2)
+                      return JSON.stringify(as_arr)
+                    }
+                  }
+
+})
---
+++ docs/yscalc.js
@@ -0,0 +1,130 @@
+var Item = /** @class */ (function () {
+    function Item() {
+    }
+    return Item;
+}());
+var Entry = /** @class */ (function () {
+    function Entry(n, i) {
+        this.name = n;
+        this.items = i;
+        this.total = 0.0;
+    }
+    Entry.prototype.push = function (v) {
+        this.items.push(v);
+    };
+    Entry.prototype.sum = function () {
+        return this.items.reduce(function (a, b) { return a + b; }, 0);
+    };
+    Entry.prototype.item_str = function () {
+        return this.items.map(function (a) { return a.toFixed(2); }).join(', ');
+    };
+    return Entry;
+}());
+function round4(tot) {
+    return Math.round(tot * 4.0) / 4.0;
+}
+function ceil4(tot) {
+    return Math.ceil(tot * 4.0 - 0.00001) / 4.0;
+}
+function price_cut(tot) {
+    if (tot < 30.0) {
+        return 0.0;
+    }
+    else if (tot < 40.0) {
+        return 30.0 - 20.0;
+    }
+    else if (tot < 70.0) {
+        return 40.0 - 25.0;
+    }
+    else if (tot < 120.0) {
+        return 70.0 - 45.0;
+    }
+    else {
+        return 120.0 - 75.0;
+    }
+}
+function yscalc(entries) {
+    var sum_before = entries.map(function (x) { return x.sum(); }).reduce(function (a, b) { return a + b; }, 0);
+    var sum_after = sum_before - price_cut(sum_before);
+    var ratio = sum_after / sum_before;
+    var pay = entries.map(function (e) { return e.sum() * ratio; });
+    var pay_rounded = pay.map(function (c) { return round4(c); });
+    var rounded_sum = pay_rounded.reduce(function (a, b) { return a + b; }, 0);
+    var remainder = ceil4(sum_after) - rounded_sum;
+    while (true) {
+        var min_idx = pay_rounded.map(function (e, i) { return [e - pay[i], i]; })
+            .reduce(function (a, b) { if (a[0] < b[0]) {
+            return a;
+        }
+        else {
+            return b;
+        } })[1];
+        var max_idx = pay_rounded.map(function (e, i) { return [e - pay[i], i]; })
+            .reduce(function (a, b) { if (a[0] > b[0]) {
+            return a;
+        }
+        else {
+            return b;
+        } })[1];
+        if (remainder > 0.0) {
+            if (Math.abs(remainder) >= 0.25) {
+                pay_rounded[min_idx] += 0.25;
+                remainder -= 0.25;
+            }
+            else {
+                pay_rounded[min_idx] += remainder;
+                remainder = 0.0;
+                break;
+            }
+        }
+        else {
+            if (Math.abs(remainder) >= 0.25) {
+                pay_rounded[max_idx] -= 0.25;
+                remainder += 0.25;
+            }
+            else {
+                // tipping the remainder
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < pay_rounded.length; i++) {
+        entries[i].total = pay_rounded[i];
+    }
+    return entries;
+}
+function testan(names, items) {
+    var dat = new Map();
+    for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+        var name = names_1[_i];
+        dat.set(name, new Entry(name, []));
+    }
+    if (items.length == 0) {
+        return [];
+    }
+    for (var _a = 0, items_1 = items; _a < items_1.length; _a++) {
+        var item = items_1[_a];
+        var v_div = void 0;
+        if (item.shared.length == 0) {
+            v_div = item.price / names.length;
+            for (var _b = 0, names_2 = names; _b < names_2.length; _b++) {
+                var name = names_2[_b];
+                dat.get(name).push(v_div);
+            }
+        }
+        else {
+            v_div = item.price / item.shared.length;
+            for (var _c = 0, _d = item.shared; _c < _d.length; _c++) {
+                var name = _d[_c];
+                dat.get(name).push(v_div);
+            }
+        }
+    }
+    var as_list = [];
+    dat.forEach(function (value, _) {
+        if (value.items.length > 0) {
+            as_list.push(value);
+        }
+    });
+    return yscalc(as_list);
+}
